d477fb5f138ef14258c50857cebeb4962107c923
cms <cms@cms-1.ge.infn.it>
2025-01-17 15:42:50 +0100

    tchau

---
+++ settings.py
@@ -0,0 +1,6 @@
+db_bucket = "CliChamber"
+db_org = "INFN-GE_test"
+db_token = "lqJKA2B35VIO3flK6zz5x2P_UgNyPK7WF3dQYGz1kIEBQOWAM0Ua06iie2ZRXkcoAo56AwU3PW2uDuly1RnPmA=="
+db_url = "http://localhost:8086"
+so_IP = "192.168.1.40"
+so_port = 2000
